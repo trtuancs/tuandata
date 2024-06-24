16bf008ccdceaa5504250405e0a97fcd39de06a4
Tuancs <tuancs@tuandata.local>
2024-06-24 08:28:32 +0700

    Finish basic website

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tuandata
+
+## Font size:
+- Title: 3xl  
+- Formal: text-lg  
+- 
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import { remarkReadingTime } from '../astro-theme-resume/src/utils/remarkReadingTime.js';
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
 import remarkUnwrapImages from 'remark-unwrap-images';
 import rehypeExternalLinks from 'rehype-external-links';
 import expressiveCode from 'astro-expressive-code';
-import { expressiveCodeOptions } from '../astro-theme-resume/src/site.config.js';
+import { expressiveCodeOptions } from './src/site.config';
 import icon from 'astro-icon';
 
 export default defineConfig({
